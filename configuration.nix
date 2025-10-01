64c4f67bf5f900eeeba43f823b6677ed842b5079
factionparadox <sophon3trisolaris@gmail.com>
2025-10-01 12:22:42 +0100

    Update configuration.nix
    I fucked up the syntax this is a known good build I'm still learning Nix-

--- configuration.nix
+++ configuration.nix
@@ -236,14 +236,16 @@
   radeontop
   powertop 
   auto-cpufreq
+];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
   programs.gnupg.agent = {
   enable = true;
-  #   enableSSHSupport = true;
-  };
+  enableSSHSupport = true;
+};
+
 
   # Hibernate Controls
    services.logind.extraConfig = ''

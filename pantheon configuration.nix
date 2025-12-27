3bc8cf8a001b9d6317fae9b571a670ba2bd68505
factionparadox <sophon3trisolaris@gmail.com>
2025-12-27 16:58:14 +0000

    Update and rename deepin configuration.nix to pantheon configuration.nix

--- pantheon configuration.nix
+++ pantheon configuration.nix
@@ -48,10 +48,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the Deepin Desktop Environment.
+  # Enable the Pantheon Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.deepin.enable = true;
-
+  services.desktopManager.pantheon.enable = true;
+  
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "gb";
